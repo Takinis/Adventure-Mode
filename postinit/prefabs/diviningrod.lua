fa2feb510a1cf62a2d6ab47b6ac876769ec6acc4
Sydney <manutsawee.louis@outlook.com>
2026-06-19 20:16:21 +0800

    fix: Correct CheckTargetPiece function to ensure proper sound handling

--- postinit/prefabs/diviningrod.lua
+++ postinit/prefabs/diviningrod.lua
@@ -8,17 +8,15 @@ AddPrefabPostInit("diviningrod", function(inst)
 
     local _CheckTargetPiece, scope_fn, i = ToolUtil.GetUpvalue(inst.components.equippable.onequipfn, "CheckTargetPiece")
     local CheckTargetPiece = function(inst)
-        local ret = { _CheckTargetPiece(inst) }
-
         -- Huh?
         if inst.components.equippable:IsEquipped() and inst.components.inventoryitem.owner then
             inst.SoundEmitter:KillSound("ping")
         end
 
-        return unpack(ret)
+        _CheckTargetPiece(inst)
     end
 
     if i then
-        debug.setupvalue(scope_fn, i, _CheckTargetPiece)
+        debug.setupvalue(scope_fn, i, CheckTargetPiece)
     end
 end)
