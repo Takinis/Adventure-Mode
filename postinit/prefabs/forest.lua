b78f1f4323e25b583931db92c238e68d009ac1a6
Ardent <47233045+Noctice@users.noreply.github.com>
2026-06-21 00:33:25 +0800

    优化了终章的地图

--- postinit/prefabs/forest.lua
+++ postinit/prefabs/forest.lua
@@ -3,9 +3,11 @@ GLOBAL.setfenv(1, GLOBAL)
 
 AddPrefabPostInit("forest", function(inst)
     if ShardGameIndex:IsAdventureActive() then
-        TheWorld.Map:AlwaysDrawWaves(true)
+        if not (ShardGameIndex:GetAdventureState().current_preset == "ENDING") then -- 终章没有海洋特效
+            TheWorld.Map:AlwaysDrawWaves(true)
+        end
         TheWorld.Map:DoOceanRender(false)
-        TheWorld.Map:SetUndergroundFadeHeight(-15)
+        TheWorld.Map:SetUndergroundFadeHeight(12) -- 看起来效果最好
         
         TheWorld.WaveComponent:SetWaveParams(13.5, 2.5)                     -- wave texture u repeat, forward distance between waves
         TheWorld.WaveComponent:SetWaveSize(80, 3.5)                         -- wave mesh width and height
--- scripts/map/ad_layouts.lua
+++ scripts/map/ad_layouts.lua
@@ -1,4 +1,20 @@
+GLOBAL.setfenv(1, GLOBAL)
+require("constants")
+
 local StaticLayout = require("map/static_layout")
 local AllLayouts = require("map/layouts").Layouts
 
 AllLayouts["MaxHomeStart"] = StaticLayout.Get("map/static_layouts/maxhome_start")
+
+AllLayouts["MaxwellHome"].force_rotation = LAYOUT_ROTATION.SOUTH
+for prefab_type, prefabs in pairs(AllLayouts["MaxwellHome"].layout) do
+    for prefab, data in pairs(prefabs) do
+        data.y = - data.y
+    end
+end
+
+local new_ground = {}
+for index, tile in pairs(AllLayouts["MaxwellHome"].ground) do
+    new_ground[150-index + 1] = tile
+end
+AllLayouts["MaxwellHome"].ground = new_ground
