7f6caa2e8d0219c78a2e82a57ef6169f94166a9b
Ardent <47233045+Noctice@users.noreply.github.com>
2026-06-21 01:15:24 +0800

    生成世界时会检测是否有冒险模式大门

--- postinit/map/tasksets/forest.lua
+++ postinit/map/tasksets/forest.lua
@@ -2,5 +2,5 @@ local AddTaskSetPreInit = AddTaskSetPreInit
 GLOBAL.setfenv(1, GLOBAL)
 
 AddTaskSetPreInit("default", function(self)
-
+    table.insert(self.required_prefabs, "adventure_portal")
 end)
