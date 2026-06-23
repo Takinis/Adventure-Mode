4df66b22e38dbe5f2e58318514636aafe04bf1f9
Sydney <manutsawee.louis@outlook.com>
2026-06-24 02:00:21 +0800

    feat: camera controller start and stop in maxwell throne scenario

--- postinit/scenarios/camera_maxwellthrone.lua
+++ postinit/scenarios/camera_maxwellthrone.lua
@@ -1,6 +1,14 @@
 local camera_maxwellthrone = require "scenarios/camera_maxwellthrone"
 GLOBAL.setfenv(1, GLOBAL)
 
-camera_maxwellthrone.OnLoad = function()
-    
+camera_maxwellthrone.OnLoad = function(inst, scenariorunner)
+    if inst ~= nil and inst.StartCameraController ~= nil then
+        inst:StartCameraController()
+    end
+end
+
+camera_maxwellthrone.OnDestroy = function(inst, scenariorunner)
+    if inst ~= nil and inst.StopCameraController ~= nil then
+        inst:StopCameraController()
+    end
 end
