f2011fab5ce9b243e956cae27f1fef47c9645fff
Sydney <manutsawee.louis@outlook.com>
2026-07-12 00:39:15 +0800

    feat: update modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,10 @@
-name = "Adventure Mode"
+local function en_zh(en, zh)
+    return (locale == "zh" or locale == "zhr" or locale == "zht") and zh or en
+end
+
+name = en_zh("Adventure Mode", "冒险模式")
 description = ""
-author = "Sydney"
+author = "Sydney, Ardent"
 forumthread = ""
 
 version = "1.0"
@@ -14,7 +18,7 @@ all_clients_require_mod = true
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
-priority = 9000
+priority = 9999
 server_filter_tags = {"adventure","adventure mode"}
 
 mod_dependencies = {}
